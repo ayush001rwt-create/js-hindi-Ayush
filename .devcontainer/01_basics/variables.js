89747662af6ce9c05a4562005cae87475be7143d
ayush001rwt-create <ayush001rwt@gmail.com>
2025-12-07 06:45:21 +0000

    container config done

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -1,1 +1,10 @@
-console.log("udjhfuj")
+const accountid = 564665
+let accountemail = "ayush@gmail.com"
+var accountpassword = "846468648aysuh"
+accountcity = "jaipur" 
+
+//accountid = 564141685
+accountemail = "dkngvidfkol@gmail.com"
+
+console.table([accountid,accountemail,accountpassword,accountcity]);
+// sun ignore var in codes do wahtever in var because tmkc//
